a122fb0bde34c589c6fb836ad3b4238214d5f09c
sabbekosu <sabbek@oregonstate.edu>
2025-10-06 22:23:28 -0700

    Completed personal website

---
+++ script.js
@@ -0,0 +1,4 @@
+function ndaNotice(title){
+  alert(title + "\n\nThis work is currently in progress and covered by an NDA. A presentation and/or code will be shared when available.");
+}
+document.getElementById('year').textContent = new Date().getFullYear();
